e6a8048d5ea6866e98321b1197bb9f8139f979ec
Kyle Stengline <kyle.stengline@idt.net>
2016-06-17 13:51:50 -0400

    adds some generic tests for presentation

--- features/my_first.feature
+++ features/my_first.feature
@@ -1,5 +1,6 @@
 Feature: Login feature
 
   Scenario: As a valid user I can log into my app
-    When I press "Login"
-    Then I see "Welcome to coolest app ever"
+    Given I am on the home page
+    When I press the Login "button"
+    Then I should see the message "Welcome to coolest app ever"
--- features/step_definitions/calabash_steps.rb
+++ features/step_definitions/calabash_steps.rb
@@ -1,1 +1,15 @@
 require 'calabash-android/calabash_steps'
+
+# Generic tests
+Given(/^I am on the home page$/) do
+  visit "/"
+end
+
+When(/^I press the Login "([^"]*)"$/) do |button|
+  click_button "Login"
+end
+
+Then(/^I should see the message "([^"]*)"$/) do |message|
+  expect(page).to have_content "Welcome"
+end
+
