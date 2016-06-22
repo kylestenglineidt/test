6da5b7f2f1ec64c678596e8d068ca190d7893e59
Kyle Stengline <kyle.stengline@idt.net>
2016-06-21 20:59:03 -0400

    adds zip file for test suite

--- features/my_first.feature
+++ features/my_first.feature
@@ -1,13 +1,6 @@
 Feature: Login feature
 
-  @login
   Scenario: As a valid user I can log into my app
-    Given I am on the login screen
-    When I press the Login "button"
-    Then I should see the message "Welcome to coolest app ever"
-  
-  @no_login
-  Scenario: As a valid user I can log into my app
-    Given I am on the login screen
-    When I press the Login "button"
+    Given I am on the Login page
+    When I press "Login" button
     Then I should see the message "Username and password do not match"
--- features/step_definitions/calabash_steps.rb
+++ features/step_definitions/calabash_steps.rb
@@ -1,17 +1,15 @@
 require 'calabash-android/calabash_steps'
 
-# Generic tests
-Given(/^I am on the login screen$/) do
- #query("button marked: 'Login'")
- #wait_for_text("Login")
+Given(/^I am on the Login page$/) do
+  
 end
 
-When(/^I press the Login "([^"]*)"$/) do |button|
- #touch "Login"
+When(/^I press "([^\"]*)" button$/) do |login|
+#byebug
+  tap_when_element_exists("android.widget.Button {text CONTAINS[c] '#{login}'}")
 end
 
-Then(/^I should see the message "([^"]*)"$/) do |message|
- #expect(page).to have_content "Welcome"
-  wait_for_text(message)
+Then(/^I should see the message "([^"]*)"$/) do |arg1|
+  
 end
 
